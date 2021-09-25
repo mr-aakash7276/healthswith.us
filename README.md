667223360b42b03fbc7ce3ebbeac2eae74c65389
Exom <19bcs1392@gmail.com>
2021-09-25 18:29:02 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+This Project is dedicated to Good Mental Health
+The main aim of this project was to spread awareness regarding Mental Health and Issues
+The Project comprises of Details regarding Mental Health including Early Signs, Myths & Facts and Dimensions of recovery
+The Highlight of the Project is the Depression Test.
+Depression Test can detect your approximate depression.
+The Depression Test/Quiz is based on JS and depends on your honest answering.
+The test purely anonymous and does not record or send anything.
+Also Feel free to give feedback
+
+[Note: The Depression Test is not Accurate and it is a basic tool to detect your depression approximately depending on your answering. This project is just a represntation of Mental Health Website 
+and is for basic awareness and by no means I guarantee the accuracy of the content and the quiz. For issues regarding mental health Always seek the advice of your mental health professional or other 
+qualified health provider with any questions you may have regarding your condition. Never disregard professional advice or delay in seeking it. Anything that might have hurt sentiments of any group or groups of people is unintentional and without malice]
+
+Technologies Used:
+
+-> HTML
+
+-> CSS
+
+-> Bootstrap
+
+-> Javascript
+
+Template Details:
+
+Template URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/
+
+Thanks to Bootstrapmade for providing such a beautiful template for Free
---
+++ quiz/questions.js
@@ -0,0 +1,114 @@
+const questions = [
+  {
+    "question": "Age range?",
+    "answer1": "under 18",
+    "answer1Total": "1",
+    "answer2": "18 - 30",
+    "answer2Total": "2",
+    "answer3": "31 - 60",
+    "answer3Total": "3",
+    "answer4": "above 60",
+    "answer4Total": "4"
+  },
+  {
+    "question": "Little interest or pleasure in doing things.",
+    "answer1": "Nearly every day",
+    "answer1Total": "4",
+    "answer2": "More than Half the Days",
+    "answer2Total": "3",
+    "answer3": "Several Days",
+    "answer3Total": "2",
+	"answer4": "Not at all",
+    "answer4Total": "1"
+  },
+  {
+    "question":
+      "Feeling down, depressed, or hopeless",
+    "answer1": "Nearly every day",
+    "answer1Total": "4",
+    "answer2": "More than Half the Days",
+    "answer2Total": "3",
+    "answer3": "Several Days",
+    "answer3Total": "2",
+	"answer4": "Not at all",
+    "answer4Total": "1"
+  },
+  {
+    "question": "Feeling tired or having little energy",
+    "answer1": "Nearly every day",
+    "answer1Total": "4",
+    "answer2": "More than Half the Days",
+    "answer2Total": "3",
+    "answer3": "Several Days",
+    "answer3Total": "2",
+	"answer4": "Not at all",
+    "answer4Total": "1"
+  },
+  {
+    "question": "Poor appetite or overeating",
+    "answer1": "Nearly every day",
+    "answer1Total": "4",
+    "answer2": "More than Half the Days",
+    "answer2Total": "3",
+    "answer3": "Several Days",
+    "answer3Total": "2",
+	"answer4": "Not at all",
+    "answer4Total": "1"
+  },
+  {
+    "question":
+      "Feeling bad about yourself - or that you are a failure or have let yourself or your family down",
+    "answer1": "Nearly every day",
+    "answer1Total": "4",
+    "answer2": "More than Half the Days",
+    "answer2Total": "3",
+    "answer3": "Several Days",
+    "answer3Total": "2",
+	"answer4": "Not at all",
+    "answer4Total": "1"
+  },
+  {
+    "question": "Trouble concentrating on things, such as reading the newspaper or watching television",
+    "answer1": "Nearly every day",
+    "answer1Total": "4",
+    "answer2": "More than Half the Days",
+    "answer2Total": "3",
+    "answer3": "Several Days",
+    "answer3Total": "2",
+	"answer4": "Not at all",
+    "answer4Total": "1"
+  },
+  {
+    "question": "Moving or speaking so slowly that other people could have noticed",
+    "answer1": "Nearly every day",
+    "answer1Total": "4",
+    "answer2": "More than Half the Days",
+    "answer2Total": "3",
+    "answer3": "Several Days",
+    "answer3Total": "2",
+	"answer4": "Not at all",
+    "answer4Total": "1"
+  },
+  {
+    "question": "Thoughts that you would be better off dead, or of hurting yourself",
+    "answer1": "Nearly every day",
+    "answer1Total": "4",
+    "answer2": "More than Half the Days",
+    "answer2Total": "3",
+    "answer3": "Several Days",
+    "answer3Total": "2",
+	"answer4": "Not at all",
+    "answer4Total": "1"
+  },
+  {
+    "question": "If you checked off any problems, how difficult have these problems made it for you at work, home, or with other people?",
+    "answer1": "Nearly every day",
+    "answer1Total": "4",
+    "answer2": "More than Half the Days",
+    "answer2Total": "3",
+    "answer3": "Several Days",
+    "answer3Total": "2",
+	"answer4": "Not at all",
+    "answer4Total": "1"
+  }
+]
